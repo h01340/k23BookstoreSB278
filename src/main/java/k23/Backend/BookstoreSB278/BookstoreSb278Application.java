9fca93a24a478ad755f728091c60f55b058c0230
Pellikka Minna <h01340@haaga-helia.fi>
2023-01-31 11:09:53 +0200

    fist commit

---
+++ src/main/java/k23/Backend/BookstoreSB278/BookstoreSb278Application.java
@@ -0,0 +1,13 @@
+package k23.Backend.BookstoreSB278;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreSb278Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreSb278Application.class, args);
+	}
+
+}
---
+++ src/test/java/k23/Backend/BookstoreSB278/BookstoreSb278ApplicationTests.java
@@ -0,0 +1,13 @@
+package k23.Backend.BookstoreSB278;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreSb278ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
